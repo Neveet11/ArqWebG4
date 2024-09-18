f58a0fd9ca7fbb04b365841e00e5287c1b9ab83b
Choxclin <108918759+Choxclin@users.noreply.github.com>
2024-09-17 21:34:43 -0500

    Creación de la clase Controladora(FORUM)

---
+++ ValorPathG4/src/main/java/pe/edu/upc/valorpathg4/controllers/ForumController.java
@@ -0,0 +1,10 @@
+package pe.edu.upc.valorpathg4.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/foros")
+public class ForumController {
+
+}
