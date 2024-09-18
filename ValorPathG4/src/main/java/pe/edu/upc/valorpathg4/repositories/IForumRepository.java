916eba6f8bc3346babe67ff4f185c3f40e7ef583
Choxclin <108918759+Choxclin@users.noreply.github.com>
2024-09-17 21:36:54 -0500

    Creación de repository(FORUM)

---
+++ ValorPathG4/src/main/java/pe/edu/upc/valorpathg4/repositories/IForumRepository.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.valorpathg4.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import pe.edu.upc.valorpathg4.entities.Forum;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Repository
+public interface IForumRepository extends JpaRepository<Forum, Integer> {
+
+}
