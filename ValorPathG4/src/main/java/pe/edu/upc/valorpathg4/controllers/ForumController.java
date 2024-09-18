cd229fb5be0ef4ea95d090250e46e6eb8ab62ce7
Choxclin <108918759+Choxclin@users.noreply.github.com>
2024-09-17 21:37:11 -0500

    Add-contenido del Controller(Forum)

--- ValorPathG4/src/main/java/pe/edu/upc/valorpathg4/controllers/ForumController.java
+++ ValorPathG4/src/main/java/pe/edu/upc/valorpathg4/controllers/ForumController.java
@@ -1,10 +1,56 @@
 package pe.edu.upc.valorpathg4.controllers;
 
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import pe.edu.upc.valorpathg4.dtos.ForumDTO;
+import pe.edu.upc.valorpathg4.entities.Forum;
+import pe.edu.upc.valorpathg4.servicesinterfaces.IForumService;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/foros")
 public class ForumController {
+    @Autowired
+    private IForumService fS;
+    @PostMapping("/registrar")
+    @PreAuthorize("hasAnyAuthority('VETERANO','PSICOLOGO')")
+    public void registrar(@RequestBody ForumDTO forumDTO) {
+        ModelMapper m = new ModelMapper();
+        Forum forum = m.map(forumDTO, Forum.class);
+        fS.insert(forum);
+    }
 
+    @PutMapping("/actualizar")
+    public void modificar(@RequestBody ForumDTO forumDTO) {
+        ModelMapper m = new ModelMapper();
+        Forum forum = m.map(forumDTO, Forum.class);
+        fS.update(forum);
+    }
+
+    @GetMapping("/listar")
+    @PreAuthorize("hasAnyAuthority('VETERANO')")
+    public List<ForumDTO> listar() {
+        return fS.list().stream().map(y -> {
+            ModelMapper m = new ModelMapper();
+            return m.map(y, ForumDTO.class);
+        }).collect(Collectors.toList());
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public void eliminar(@PathVariable("id") Integer id) {
+        fS.delete(id);
+    }
+
+    @GetMapping("/listar/{id}")
+    public ForumDTO listarPorId(@PathVariable("id") Integer id) {
+        ModelMapper m = new ModelMapper();
+        ForumDTO forumDTO = m.map(fS.listId(id), ForumDTO.class);
+        return forumDTO;
+    }
 }
+
---
+++ ValorPathG4/src/main/java/pe/edu/upc/valorpathg4/servicesinterfaces/IForumService.java
@@ -0,0 +1,15 @@
+package pe.edu.upc.valorpathg4.servicesinterfaces;
+
+import pe.edu.upc.valorpathg4.entities.Forum;
+
+
+import java.util.List;
+
+public interface IForumService {
+    public void insert (Forum forum);
+    public void update (Forum forum);
+    public void delete (int id);
+    public List<Forum> list();
+    public Forum listId (int id);
+    //agregar los otros US que sirvan
+}
